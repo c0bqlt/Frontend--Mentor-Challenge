7fd6b5cfaae13a5641052cea2df5198bcb440993
c0bqlt <103998296+c0bqlt@users.noreply.github.com>
2022-05-11 20:02:37 +0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+var rating = 0;
+
 function storeVar(el) {
-    var rating =el.getAttribute('value');
-    console.log(rating);
+    var element = document.getElementById("selected-button");
+    if (element) {
+        document.getElementById("selected-button").removeAttribute("id");
+    }
+    rating =el.getAttribute('value');
+    el.setAttribute('id','selected-button');
+    document.cookie = rating;
+}
+
+function nextPage() {
+    if (rating !=0) {
+        window.location.href = 'thank-you.html'
+    }
+    else {
+        alert("Please select a rating to submit");
+    }
 }
